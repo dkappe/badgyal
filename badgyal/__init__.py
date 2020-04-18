de7bfcc2077e35e4df4b526560fa3af436089165
Dietrich Kappe <dkappe@gmail.com>
2020-04-18 00:51:13 -0500

    Added wdl refactor and GG and ME nets

--- badgyal/__init__.py
+++ badgyal/__init__.py
@@ -1,6 +1,9 @@
 from badgyal.abstractnet import AbstractNet
+from badgyal.wdlnet import WDLNet
 from badgyal.bgnet import BGNet
+from badgyal.ggnet import GGNet
 from badgyal.mgnet import MGNet
 from badgyal.lenet import LENet
+from badgyal.menet import MENet
 from badgyal.policy_index import policy_index
 from badgyal.board2planes import board2planes, bulk_board2planes
--- badgyal/lenet.py
+++ badgyal/lenet.py
@@ -7,14 +7,14 @@ from badgyal.board2planes import board2planes, policy2moves, bulk_board2planes
 import pylru
 import sys
 import os.path
-from badgyal import AbstractNet
+from badgyal import WDLNet
 import math
 
 CHANNELS=128
 BLOCKS=10
 SE=4
 
-class LENet(AbstractNet):
+class LENet(WDLNet):
 
     def __init__(self, cuda=True, torchScript=False):
         super().__init__(cuda=cuda, torchScript=torchScript)
@@ -25,14 +25,3 @@ class LENet(AbstractNet):
         net = model.Net(CHANNELS, BLOCKS, CHANNELS, SE)
         net.import_proto_classical(file)
         return net
-
-    def value_to_scalar(self, value):
-        wdl0 = value[0].item()
-        wdl1 = value[1].item()
-        wdl2 = value[2].item()
-        min_val = min(wdl0, wdl1, wdl2)
-        w_val = math.exp(wdl0 - min_val)
-        d_val = math.exp(wdl1 - min_val)
-        l_val = math.exp(wdl2 - min_val)
-        p = (w_val * 1.0 + d_val * 0.5 ) / (w_val + d_val + l_val)
-        return 2.0*p-1.0;
